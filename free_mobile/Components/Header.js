1667f29f09d9a5c3a59023bb2c4b0756cc23b38f
Joey Stout <joey@apollorion.com>
2020-02-15 22:34:22 -0500

    code cleanup

--- free_mobile/Components/Header.js
+++ free_mobile/Components/Header.js
@@ -3,20 +3,19 @@ import { Header as RneHeader } from 'react-native-elements';
 
 export default class Header extends React.Component {
     render(){
+
+        let icon = "menu";
+        let navigateTo = "Menu";
         if(this.props.back){
-            return (
-                <RneHeader
-                    leftComponent={{ icon: 'arrow-back', color: '#fff', onPress: () => { this.props.navigation.navigate('Home') } }}
-                    centerComponent={{ text: 'Free Speech', style: { color: '#fff' } }}
-                />
-            );
-        } else {
-            return (
-                <RneHeader
-                    leftComponent={{ icon: 'menu', color: '#fff', onPress: () => { this.props.navigation.navigate('Menu') } }}
-                    centerComponent={{ text: 'Free Speech', style: { color: '#fff' } }}
-                />
-            );
+            icon = "arrow-back";
+            navigateTo = "Home";
         }
+
+        return (
+            <RneHeader
+                leftComponent={{ icon: icon, color: '#fff', onPress: () => { this.props.navigation.navigate(navigateTo) } }}
+                centerComponent={{ text: 'Free Speech', style: { color: '#fff' } }}
+            />
+        );
     }
 }
