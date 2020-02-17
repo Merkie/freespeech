01ffc1923653d9e03306ba1592ea91077a58f512
Bailey Townsend <baileytownsend2323@gmail.com>
2020-02-17 13:31:30 -0600

    Gitiginore Changes and nodemodules removal

--- vueapp/node_modules/vuetify/src/components/VCalendar/modes/index.ts
+++
@@ -1,8 +0,0 @@
-import { CalendarEventOverlapMode } from 'types'
-import { stack } from './stack'
-import { column } from './column'
-
-export const CalendarEventOverlapModes: Record<string, CalendarEventOverlapMode> = {
-  stack,
-  column,
-}
