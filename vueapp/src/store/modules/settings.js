8163e18e76d436b25f5fe7accb73f98fa0f1f6e1
James Burnham <labpd01@googlemail.com>
2020-02-19 17:59:30 +0000

    Formatting for mutations and actions

--- vueapp/src/store/modules/settings.js
+++ vueapp/src/store/modules/settings.js
@@ -9,43 +9,43 @@ const state = {
 
 const mutations = {
   SET_SELECTED_VOICE_INDEX(state, value) {
-      state.selectedVoiceIndex = value;
+    state.selectedVoiceIndex = value;
   },
   SET_VOICES(state, value) {
-      state.voices = value;
+    state.voices = value;
   },
   SET_VOICE_OPTIONS(state, value) {
-      state.voiceOptions = value;
+    state.voiceOptions = value;
   },
   TOGGLE_SETTINGS_DIALOG_VISIBILITY(state) {
-      state.settingsDialogVisibility = !state.settingsDialogVisibility;
+    state.settingsDialogVisibility = !state.settingsDialogVisibility;
   },
   TOGGLE_CUSTOM_TILE_PAD(state) {
-      state.customTilePad = !state.customTilePad;
+    state.customTilePad = !state.customTilePad;
   },
   TOGGLE_SENTENCE_MODE(state) {
-      state.sentenceMode = !state.sentenceMode;
+    state.sentenceMode = !state.sentenceMode;
   },
 };
 
 const actions = {
   toggleSentenceMode: ({ commit }) => {
-      commit('TOGGLE_SENTENCE_MODE');
+    commit('TOGGLE_SENTENCE_MODE');
   },
   setSelectedVoiceIndex: ({ commit }, value) => {
-      commit('SET_SELECTED_VOICE_INDEX', value);
+    commit('SET_SELECTED_VOICE_INDEX', value);
   },
   setVoices: ({ commit }, value) => {
-      commit('SET_VOICES', value);
+    commit('SET_VOICES', value);
   },
   setVoiceOptions: ({ commit }, value) => {
-      commit('SET_VOICE_OPTIONS', value);
+    commit('SET_VOICE_OPTIONS', value);
   },
   toggleSettingsDialogVisibility: ({ commit }) => {
-      commit('TOGGLE_SETTINGS_DIALOG_VISIBILITY');
+    commit('TOGGLE_SETTINGS_DIALOG_VISIBILITY');
   },
   toggleCustomTilePad: ({ commit }) => {
-      commit('TOGGLE_CUSTOM_TILE_PAD');
+    commit('TOGGLE_CUSTOM_TILE_PAD');
   }
 };
 
