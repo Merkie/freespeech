405320ea306c0c3abbe8f418b7964eaa55207f16
José Guadalupe Galván Aguilar <jgalvan@itexico.com>
2020-04-04 15:31:17 -0600

    Fix unit test SaveChangesButton

--- vueapp/src/components/EditModeHeader/EditModeHeader.vue
+++ vueapp/src/components/EditModeHeader/EditModeHeader.vue
@@ -1,31 +1,43 @@
 <template>
-  <div class="sentenceCardContainer grey lighten-5">
-    <v-card outlined class="px-4 sentenceCard">
-      <v-card-title>
-        {{ $t('editMode.title') }}
-        <v-spacer />
-        <v-btn id="exitEditModeButton" class="mx-2" color="success" @click.native="exitEditMode">
-          <v-icon class="pr-2">save</v-icon>
-          {{ $t('editMode.buttonSave') }}
-        </v-btn>
-      </v-card-title>
-      <v-container class="py-0">
-        <p class="body-2">{{ $t('editMode.instructions') }}</p>
-      </v-container>
-    </v-card>
-  </div>
+	<div class="sentenceCardContainer grey lighten-5">
+		<v-card
+			outlined
+			class="px-4 sentenceCard"
+		>
+			<v-card-title>
+				{{ $t('editMode.title') }}
+				<v-spacer />
+				<v-btn
+					id="exitEditModeButton"
+					class="mx-2"
+					color="success"
+					@click.native="exitEditMode"
+				>
+					<v-icon class="pr-2">
+						save
+					</v-icon>
+					{{ $t('editMode.buttonSave') }}
+				</v-btn>
+			</v-card-title>
+			<v-container class="py-0">
+				<p class="body-2">
+					{{ $t('editMode.instructions') }}
+				</p>
+			</v-container>
+		</v-card>
+	</div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions } from 'vuex';
 
 export default {
-  methods: {
-    ...mapActions("tilePad", ["setEditMode"]),
-    exitEditMode() {
-      this.setEditMode(false);
-    }
-  }
+	methods: {
+		...mapActions('tilePad', ['setEditMode']),
+		exitEditMode() {
+			this.setEditMode(false);
+		}
+	}
 };
 </script>
 <style scoped>
