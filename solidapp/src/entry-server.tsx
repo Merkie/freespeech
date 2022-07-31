5d3bfb2cca9cdf5b2742cf4bc27392af2fad2621
Merkie <merkie@post.com>
2022-07-31 16:05:22 -0500

    remove solidapp

--- solidapp/src/entry-server.tsx
+++
@@ -1,9 +0,0 @@
-import {
-  StartServer,
-  createHandler,
-  renderAsync,
-} from "solid-start/entry-server";
-
-export default createHandler(
-  renderAsync((context) => <StartServer context={context} />)
-);
--- solidapp/src/types/AppTypes.ts
+++
@@ -1,3 +0,0 @@
-export interface ILocalStorage {
-  session?: string;
-}
