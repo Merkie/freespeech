133629aee4a4bb4c479c8f96a760ef955226857d
Merkie <merkie@post.com>
2022-08-14 11:49:11 -0500

    migrate to new stack

--- src/lib/user/logout.ts
+++
@@ -1,7 +0,0 @@
-export async function logout(token: string) {
-  // Send request
-  const response = await fetch("/api/user/logout", {
-    method: "POST",
-    body: JSON.stringify({ token: token }),
-  });
-}
