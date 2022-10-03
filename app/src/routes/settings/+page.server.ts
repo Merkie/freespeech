8a5069461674faef1e4f867adfa1f00c23c95989
Merkie <merkie@post.com>
2022-10-03 16:47:05 -0500

    remove settings page

--- app/src/routes/settings/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { ServerLoad } from '@sveltejs/kit';
-
-export const load: ServerLoad = async ({ parent }) => {
-	const auth = await parent();
-	if (!auth['_lucia']) throw redirect(302, '/');
-};
