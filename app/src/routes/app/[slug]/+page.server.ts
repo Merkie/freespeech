de3352f197cc272d7c9483933759154023baafa5
Merkie <merkie@post.com>
2022-10-13 21:18:09 -0500

    type fixes in backend+timer

--- app/src/routes/app/[slug]/+page.server.ts
+++ app/src/routes/app/[slug]/+page.server.ts
@@ -3,11 +3,16 @@ import trpc from '$lib/client/trpc';
 import type { TilePage, Project, Tile } from '@prisma/client';
 
 export const load: Load = async ({ params, fetch }) => {
+	const start_time = Date.now();
 	// 1) Get slug
 	const { slug } = params;
 
 	// 2) Get the project from trpc
-	const project = await trpc(fetch).query('project:fetch', slug + '');
+	//@ts-ignore
+	const project: Project & { pages: (TilePage & { tiles: Tile[] })[] } = await trpc(fetch).query(
+		'project:fetch',
+		slug + ''
+	);
 	if (!project) return { error: 404 };
 
 	// 3) Sort the tiles by their tile_index property
@@ -29,33 +34,49 @@ export const load: Load = async ({ params, fetch }) => {
 				delete new_tile.userId;
 				delete new_tile.tilePageId;
 				delete new_tile.tile_index;
-				new_project.pages[new_project.pages.indexOf(page)].tiles[
-					new_project.pages[new_project.pages.indexOf(page)].tiles.indexOf(tile)
-				] = { ...tile, ...new_tile, tile_index: index, link_id: new_tile_id };
-				// update with trpc
-				await trpc(fetch).mutation('tile:edit', {
-					id: tile.id,
+
+				const corrected_tile: Tile = {
+					//@ts-ignore
 					...tile,
 					...new_tile,
 					tile_index: index,
 					link_id: new_tile_id
-				});
+				};
+
+				new_project.pages[new_project.pages.indexOf(page)].tiles[
+					new_project.pages[new_project.pages.indexOf(page)].tiles.indexOf(tile)
+				] = corrected_tile;
+
+				// update with trpc
+				if (tile !== corrected_tile) {
+					await trpc(fetch).mutation('tile:edit', {
+						...tile,
+						...new_tile,
+						tile_index: index,
+						link_id: new_tile_id,
+						id: tile.id
+					});
+				}
 			}
 			// temp: force sensable tile order
 			try {
 				new_project.pages[new_project.pages.indexOf(page)].tiles[
 					new_project.pages[new_project.pages.indexOf(page)].tiles.indexOf(tile)
 				].tile_index = index;
-				// update with trpc
-				await trpc(fetch).mutation('tile:edit', {
-					id: tile.id,
-					...tile,
-					tile_index: index
-				});
+				if (tile.tile_index != index) {
+					await trpc(fetch).mutation('tile:edit', {
+						...tile,
+						tile_index: index,
+						id: tile.id
+					});
+				}
 				index++;
 			} catch (e) {}
 		}
 	}
 
+	const end_time = Date.now();
+	let timeDiffInSeconds = Math.floor((end_time - start_time) / 1000);
+	console.log('Load time: ', timeDiffInSeconds + 's', Date.now());
 	return new_project;
 };
