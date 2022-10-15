e6d20e4157c41479bd108830334e1c47897e13f3
Merkie <merkie@post.com>
2022-10-15 17:17:48 -0500

    service worker fix

--- app/src/service-worker.ts
+++ app/src/service-worker.ts
@@ -71,16 +71,3 @@ worker.addEventListener('fetch', (event) => {
 		);
 	}
 });
-
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-	// Prevents the default mini-infobar or install dialog from appearing on mobile
-	e.preventDefault();
-	// Save the event because you'll need to trigger it later.
-	deferredPrompt = e;
-	// Show your customized install prompt for your PWA
-	// Your own UI doesn't have to be a single element, you
-	// can have buttons in different locations, or wait to prompt
-	// as part of a critical journey.
-	// showInAppInstallPromotion();
-});
