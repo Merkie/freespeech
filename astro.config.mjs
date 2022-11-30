c23de8b711864412a9c6b3e0de3a5761b8fccf3e
Merkie <merkie@post.com>
2022-11-30 16:59:54 -0600

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), svelte()],
+  adapter: vercel()
 });
