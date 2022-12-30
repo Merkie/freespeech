bc1f79bea2557c78ca6319a894e8f911f39fd802
Merkie <merkie@post.com>
2022-12-30 12:15:24 -0600

    netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+// import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [solidJs(), tailwind(), svelte()],
+  integrations: [tailwind(), svelte()],
   adapter: netlify()
 });
