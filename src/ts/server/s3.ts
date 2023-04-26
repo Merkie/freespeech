f08940eac90008b925bc6cda8da24a28f5dddca1
Merkie <merkie@post.com>
2023-04-26 16:01:25 -0500

    reconfigure S3

--- src/ts/server/s3.ts
+++ src/ts/server/s3.ts
@@ -1,12 +1,5 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY, S3_REGION } from '$env/static/private';
 
-const client = new S3Client({
-	region: S3_REGION,
-	credentials: {
-		accessKeyId: S3_ACCESS_KEY,
-		secretAccessKey: S3_SECRET_ACCESS_KEY
-	}
-});
+const client = new S3Client({});
 
 export default client;
