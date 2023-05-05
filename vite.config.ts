af43e3e6feb76e15e53751e822cd52284a6a6663
Merkie <merkie@post.com>
2023-05-05 13:24:51 -0500

    adjust dependency structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
+// import { VitePWA } from 'vite-plugin-pwa';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit()
+		// VitePWA({
+		// 	registerType: 'autoUpdate',
+		// 	manifest: {
+		// 		name: 'FreeSpeech AAC',
+		// 		short_name: 'FreeSpeech',
+		// 		description: 'Free & Open-Source AAC, For Everyone.',
+		// 		theme_color: '#ffffff',
+		// 		icons: [
+		// 			{
+		// 				src: 'favicon.png',
+		// 				sizes: '192x192',
+		// 				type: 'image/png'
+		// 			},
+		// 			{
+		// 				src: 'favicon.png',
+		// 				sizes: '512x512',
+		// 				type: 'image/png'
+		// 			}
+		// 		]
+		// 	}
+		// })
+	]
 });
