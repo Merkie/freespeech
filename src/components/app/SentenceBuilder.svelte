178806571f8aa98f143850d6c159357ca461ce23
Merkie <merkie@post.com>
2023-09-05 13:08:16 -0500

    remove labels from speak and trash buttons

--- src/components/app/SentenceBuilder.svelte
+++ src/components/app/SentenceBuilder.svelte
@@ -20,51 +20,54 @@
 </script>
 
 <div
-	class="p-2 relative bg-zinc-50 min-h-[100px] border-b border-zinc-200 flex items-center gap-2 rounded-md overflow-hidden"
+	class="relative flex min-h-[100px] items-center gap-2 overflow-hidden rounded-md border-b border-zinc-200 bg-zinc-50 p-2"
 >
 	{#if $isSynthesizingSpeech}
 		<div
 			in:fade
-			class="absolute z-20 top-0 left-0 bg-[rgba(0,0,0,0.5)] w-full h-full grid place-items-center"
+			class="absolute left-0 top-0 z-20 grid h-full w-full place-items-center bg-[rgba(0,0,0,0.5)]"
 		>
 			<div in:scale>
 				<SynthesisLoader />
 			</div>
 		</div>
 	{/if}
-	<div class="flex items-center h-full overflow-x-auto flex-1 gap-2">
+	<div class="flex h-full flex-1 items-center gap-2 overflow-x-auto">
 		{#each $Sentence as tile, index}
 			<button
 				on:click={() => {
 					$Sentence = $Sentence.filter((_, i) => i !== index);
 				}}
 				transition:scale={{ duration: 100 }}
-				class={`p-1 min-w-[100px] w-fit h-[70px] border ${getColorClasses(
+				class={`h-[70px] w-fit min-w-[100px] border p-1 ${getColorClasses(
 					tile.color
-				)} rounded-md flex flex-col items-center overflow-hidden ${
+				)} flex flex-col items-center overflow-hidden rounded-md ${
 					!tile.image ? 'justify-center' : ''
 				}`}
 			>
 				<p class="text-ellipsis py-1 text-sm">{tile.displayText || tile.text}</p>
 				{#if tile.image}
-					<div class="flex-1 w-full relative">
-						<img src={tile.image} class="absolute w-full h-full object-contain" alt="Tile media" />
+					<div class="relative w-full flex-1">
+						<img src={tile.image} class="absolute h-full w-full object-contain" alt="Tile media" />
 					</div>
 				{/if}
 			</button>
 		{/each}
 	</div>
-	<div class="flex items-center h-full gap-2">
+	<div class="flex h-full items-center gap-2">
 		<button
 			on:click={() => speakText($Sentence.map((tile) => tile.text).join(' '))}
-			class="p-2 px-4 bg-blue-500 rounded-md text-blue-50"
+			class="grid h-[80px] w-[80px] place-items-center rounded-md bg-blue-500 text-blue-50"
 		>
-			<i class="text-2xl bi bi-volume-up-fill" />
-			<p class="text-sm">Speak</p>
+			<i class="bi bi-volume-up-fill text-4xl" />
+			<!-- <p class="text-sm">Speak</p> -->
 		</button>
-		<button on:click={() => ($Sentence = [])} class="p-2 px-4 bg-red-500 rounded-md text-blue-50">
-			<i class="text-2xl bi bi-trash-fill" />
-			<p class="text-sm">Clear</p>
+		<button
+			on:click={() => ($Sentence = [])}
+			class="grid h-[80px] w-[80px] place-items-center rounded-md bg-red-500 text-blue-50"
+		>
+			<i class="bi bi-trash-fill text-4xl" />
+			<!-- <p class="text-sm">Clear</p> -->
 		</button>
 	</div>
 </div>
