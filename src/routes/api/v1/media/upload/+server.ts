77441008fb992073b4074d9b5828b89ab158d4dd
Merkie <merkie@post.com>
2023-09-12 15:06:08 -0500

    update /api/v1/media/upload

--- src/routes/api/v1/media/upload/+server.ts
+++ src/routes/api/v1/media/upload/+server.ts
@@ -1,56 +1,42 @@
-import { MediaUploadSchema } from '$ts/common/schema';
 import { z } from 'zod';
-import { R2_BUCKET } from '$env/static/private';
+import { R2_BUCKET, R2_PUBLIC_URL } from '$env/static/private';
 import stringGate from '$ts/common/stringGate';
-
 import s3 from '$ts/server/s3';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { superValidate } from 'sveltekit-superforms/server';
+import { json } from '@sveltejs/kit';
 
 export const POST = async ({ request, locals }) => {
-	// Check if the user is logged in
-	if (!locals.user)
-		return new Response(JSON.stringify({ error: 'You must be logged in to upload media.' }), {
-			status: 401
-		});
+	const form = await superValidate(
+		await request.json(),
+		z.object({
+			filename: z.string().min(1),
+			base64data: z.string().min(1)
+		})
+	);
 
-	// Validate the request body
-	let body;
-	try {
-		body = MediaUploadSchema.parse(await request.json());
-	} catch (err) {
-		if (err instanceof z.ZodError) {
-			return new Response(JSON.stringify({ error: 'An error occured when validating form.' }), {
-				status: 400
-			});
-		}
-		return new Response(JSON.stringify({ error: 'An unknown error occured.' }), {
-			status: 500
-		});
-	}
+	if (!form.valid) return json({ error: 'Invalid form' });
 
 	// Create the media path
 	const key = `${stringGate(locals.user.name)}-${locals.user.id}/${Date.now()}-${stringGate(
-		body.filename
+		form.data.filename
 	)}`;
 
 	const putObjectCommand = new PutObjectCommand({
 		Bucket: R2_BUCKET,
 		Key: key,
-		Body: Buffer.from(body.base64data, 'base64')
+		Body: Buffer.from(form.data.base64data, 'base64')
 	});
 
 	const putObjectResponse = await s3.send(putObjectCommand);
 
 	if (putObjectResponse.$metadata.httpStatusCode !== 200)
-		return new Response(
-			JSON.stringify({ error: 'An error occured when uploading the media to storage.' }),
-			{
-				status: 500
-			}
-		);
+		return json({
+			error: 'An error occured when uploading the media to storage.'
+		});
 
 	// Check if the upload was successful
-	const fileurl = `https://pub-3aabe8e9655b4a5eb94c0efbaa7142a1.r2.dev/${key}`;
+	const fileurl = `${R2_PUBLIC_URL}${key}`;
 
 	// Add the media to the database
 	await locals.prisma.userMedia.create({
@@ -65,7 +51,5 @@ export const POST = async ({ request, locals }) => {
 	});
 
 	// Return the file URL
-	return new Response(JSON.stringify({ fileurl }), {
-		status: 200
-	});
+	return json({ fileurl });
 };
