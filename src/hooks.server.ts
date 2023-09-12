67babbfd6368de81c2522e12b82f512fcaf172a9
Merkie <merkie@post.com>
2023-09-12 10:04:29 -0500

    prisma fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,11 +6,20 @@ import type { User } from '@prisma/client';
 export async function handle({ event, resolve }) {
 	const cookie = event.cookies.get('token');
 
+	// Add prisma to the locals
+	event.locals.prisma = prisma;
+
 	// Decode the token and add user to locals
-	if(cookie) {
-		const decodedToken = jwt.verify(cookie, JWT_SECRET) as { id: string };
+	if (cookie) {
+		let decodedToken: { id: string };
+		try {
+			decodedToken = jwt.verify(cookie, JWT_SECRET) as { id: string };
+		} catch (e) {
+			event.locals.user = undefined;
+			return await resolve(event);
+		}
 		const fetchedUser = await prisma.user.findUnique({ where: { id: decodedToken.id } });
-		if(fetchedUser) {
+		if (fetchedUser) {
 			delete (fetchedUser as { password?: string }).password;
 			event.locals.user = fetchedUser as Omit<User, 'password'>;
 		} else {
@@ -19,9 +28,6 @@ export async function handle({ event, resolve }) {
 	} else {
 		event.locals.user = undefined;
 	}
-
-	// Add prisma to the locals
-	event.locals.prisma = prisma;
 
 	return await resolve(event);
 }
