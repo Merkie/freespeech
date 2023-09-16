934797dd0e4292afd8da08e47c261cf2ab8d4aa5
Merkie <merkie@post.com>
2023-09-16 15:21:46 -0500

    remove image deletion

--- src/routes/app/dashboard/profile/+page.server.ts
+++ src/routes/app/dashboard/profile/+page.server.ts
@@ -5,86 +5,86 @@ import { DeleteObjectCommand } from '@aws-sdk/client-s3';
 import Cryptr from 'cryptr';
 
 export const load = async ({ locals: { user, prisma } }) => {
-	if (!user) return { files: [] };
+	// if (!user) return { files: [] };
 
-	const deleteFile = async (url: string) => {
-		const media = await prisma.userMedia.findFirst({
-			where: {
-				url: url,
-				userId: user?.id
-			}
-		});
+	// const deleteFile = async (url: string) => {
+	// 	const media = await prisma.userMedia.findFirst({
+	// 		where: {
+	// 			url: url,
+	// 			userId: user?.id
+	// 		}
+	// 	});
 
-		// Extract the key from the media URL
-		const key = url.replace(/^https:\/\/pub-3aabe8e9655b4a5eb94c0efbaa7142a1\.r2\.dev\//, '');
+	// 	// Extract the key from the media URL
+	// 	const key = url.replace(/^https:\/\/pub-3aabe8e9655b4a5eb94c0efbaa7142a1\.r2\.dev\//, '');
 
-		const deleteObjectCommand = new DeleteObjectCommand({
-			Bucket: R2_BUCKET,
-			Key: key
-		});
+	// 	const deleteObjectCommand = new DeleteObjectCommand({
+	// 		Bucket: R2_BUCKET,
+	// 		Key: key
+	// 	});
 
-		const deleteObjectResponse = await s3.send(deleteObjectCommand);
+	// 	const deleteObjectResponse = await s3.send(deleteObjectCommand);
 
-		// Check if the media was successfully deleted
-		if (deleteObjectResponse.$metadata.httpStatusCode !== 204) return false;
+	// 	// Check if the media was successfully deleted
+	// 	if (deleteObjectResponse.$metadata.httpStatusCode !== 204) return false;
 
-		// Delete the media from the database
-		await prisma.userMedia.delete({
-			where: {
-				id: media?.id
-			}
-		});
+	// 	// Delete the media from the database
+	// 	await prisma.userMedia.delete({
+	// 		where: {
+	// 			id: media?.id
+	// 		}
+	// 	});
 
-		return true;
-	};
+	// 	return true;
+	// };
 
-	// Query the database for all the user's media files
-	const userMedia = await prisma.userMedia.findMany({
-		where: {
-			user: {
-				id: user.id
-			}
-		}
-	});
+	// // Query the database for all the user's media files
+	// const userMedia = await prisma.userMedia.findMany({
+	// 	where: {
+	// 		user: {
+	// 			id: user.id
+	// 		}
+	// 	}
+	// });
 
-	// Fetch all projects from the user
-	const userProjects = await prisma.project.findMany({
-		where: {
-			user: {
-				id: user.id
-			}
-		},
-		include: {
-			pages: true
-		}
-	});
+	// // Fetch all projects from the user
+	// const userProjects = await prisma.project.findMany({
+	// 	where: {
+	// 		user: {
+	// 			id: user.id
+	// 		}
+	// 	},
+	// 	include: {
+	// 		pages: true
+	// 	}
+	// });
 
-	// Get all the image URLs from the user's projects
-	const imageUrls: string[] = userProjects.flatMap((project) =>
-		project.pages.flatMap(
-			(page) => (page as TilePage)?.data?.tiles.flatMap((tile) => tile?.image || [])
-		)
-	);
+	// // Get all the image URLs from the user's projects
+	// const imageUrls: string[] = userProjects.flatMap((project) =>
+	// 	project.pages.flatMap(
+	// 		(page) => (page as TilePage)?.data?.tiles.flatMap((tile) => tile?.image || [])
+	// 	)
+	// );
 
-	const thumbnailUrls = userProjects.map((project) => {
-		if (project.imageUrl) return project.imageUrl;
-	});
+	// const thumbnailUrls = userProjects.map((project) => {
+	// 	if (project.imageUrl) return project.imageUrl;
+	// });
 
-	const mediaInUse = [...imageUrls, ...thumbnailUrls, user.profileImgUrl];
+	// const mediaInUse = [...imageUrls, ...thumbnailUrls, user.profileImgUrl];
 
-	// Collect all the media URLs that are not in use
-	const unusedMedia = userMedia.filter((media) => !mediaInUse.includes(media.url));
+	// // Collect all the media URLs that are not in use
+	// const unusedMedia = userMedia.filter((media) => !mediaInUse.includes(media.url));
 
-	// Delete all unused media using Promise.all
-	const deletionResults = await Promise.all(
-		unusedMedia.map(async (media) => {
-			const success = await deleteFile(media.url);
-			return success;
-		})
-	);
+	// // Delete all unused media using Promise.all
+	// const deletionResults = await Promise.all(
+	// 	unusedMedia.map(async (media) => {
+	// 		const success = await deleteFile(media.url);
+	// 		return success;
+	// 	})
+	// );
 
-	// Count the number of deletions that failed
-	const failures = deletionResults.filter((result) => !result).length;
+	// // Count the number of deletions that failed
+	// const failures = deletionResults.filter((result) => !result).length;
 
 	let elevenLabsApiKey = '';
 
@@ -93,5 +93,5 @@ export const load = async ({ locals: { user, prisma } }) => {
 		elevenLabsApiKey = cryptr.decrypt(user.elevenLabsApiKey);
 	}
 
-	return { failures, elevenLabsApiKey };
+	return { elevenLabsApiKey };
 };
