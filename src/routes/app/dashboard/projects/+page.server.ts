470b5d6f7ffbc37c6a30aedf3b5bcdf71475185f
Merkie <merkie@post.com>
2023-11-12 15:00:10 -0600

    remove non-existent package

--- src/routes/app/dashboard/projects/+page.server.ts
+++ src/routes/app/dashboard/projects/+page.server.ts
@@ -1,14 +1,13 @@
-import { z } from 'zod';
-import { superValidate } from 'sveltekit-superforms/server';
+// import { z } from 'zod';
 import { redirect } from '@sveltejs/kit';
 
-const CreateNewProjectSchema = z.object({
-	name: z.string().min(1),
-	columns: z.number().default(6).optional(),
-	rows: z.number().default(4).optional(),
-	description: z.string().optional(),
-	isPublic: z.boolean().optional()
-});
+// const CreateNewProjectSchema = z.object({
+// 	name: z.string().min(1),
+// 	columns: z.number().default(6).optional(),
+// 	rows: z.number().default(4).optional(),
+// 	description: z.string().optional(),
+// 	isPublic: z.boolean().optional()
+// });
 
 export const load = async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/login');
@@ -23,9 +22,9 @@ export const load = async ({ locals }) => {
 		}
 	});
 
-	const createProjectForm = await superValidate(CreateNewProjectSchema, {
-		id: 'createForm'
-	});
+	// const createProjectForm = await superValidate(CreateNewProjectSchema, {
+	// 	id: 'createForm'
+	// });
 
-	return { projects, createProjectForm };
+	return { projects };
 };
