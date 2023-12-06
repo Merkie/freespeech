dab0bf84acad19991fdb701800f1961705729f4b
Merkie <merkie@post.com>
2023-12-06 17:46:02 -0600

    add common opensymbols file for pigments

---
+++ src/ts/common/opensymbols.ts
@@ -0,0 +1,22 @@
+export const SkinTones = [
+	{
+		name: 'dark',
+		pigmentHexValue: '#774837'
+	},
+	{
+		name: 'medium-dark',
+		pigmentHexValue: '#af7450'
+	},
+	{
+		name: 'medium',
+		pigmentHexValue: '#d7a481'
+	},
+	{
+		name: 'medium-light',
+		pigmentHexValue: '#f6cc9d'
+	},
+	{
+		name: 'light',
+		pigmentHexValue: '#fadbca'
+	}
+];
