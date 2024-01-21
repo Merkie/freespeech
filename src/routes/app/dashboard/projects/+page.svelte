5d2cf38338698aa843b1c2b42ac4da3976d58496
Merkie <merkie@post.com>
2024-01-21 13:16:00 -0600

    Refactor project page component

--- src/routes/app/dashboard/projects/+page.svelte
+++ src/routes/app/dashboard/projects/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-	import ProjectCard from '$components/dashboard/ProjectCard.svelte';
-	import CreateProjectModal from '$components/modals/CreateProjectModal.svelte';
 	import { onMount } from 'svelte';
-	import type { PageData } from './$types';
-	import { fly } from 'svelte/transition';
-	import type { Project } from '@prisma/client';
-	import Fuse from 'fuse.js';
-	import { openModal } from '$ts/client/stores';
-	import SearchBar from '$components/dashboard/SearchBar.svelte';
 	import { writable } from 'svelte/store';
+	import { fly } from 'svelte/transition';
+	import Fuse from 'fuse.js';
+	import type { Project } from '@prisma/client';
+	import { openModal } from '$ts/client/stores';
+	import ProjectCard from '$components/dashboard/ProjectCard.svelte';
+	import SearchBar from '$components/dashboard/SearchBar.svelte';
 	import ManageProjectsModal from '$components/modals/ManageProjectsModal.svelte';
-	export let data: PageData;
+	import CreateProjectModal from '$components/modals/CreateProjectModal.svelte';
+
+	export let data;
 
 	let searchQuery = writable('');
 	let searchedProjects: Project[] = [];
-	let editModeOn = false;
 
 	onMount(() => {
 		// Open the create project modal if there are no projects
@@ -42,19 +41,24 @@
 <SearchBar query={searchQuery}>
 	<button
 		on:click={() => ($openModal = { name: 'create-project' })}
-		class="hidden rounded-md border border-blue-500 bg-blue-600 p-2 px-4 text-sm text-blue-50 sm:block"
-		><i class="bi bi-plus-lg" /> Create New Project</button
+		class="btn-sm btn-primary hidden sm:flex"
 	>
+		<i class="bi bi-plus-lg" />
+		<span>Create New Project</span>
+	</button>
 	<button
 		on:click={() => ($openModal = { name: 'manage-projects', props: { projects: data.projects } })}
-		class="hidden rounded-md border border-zinc-600 bg-zinc-700 p-2 px-4 text-sm text-zinc-50 sm:block"
-		><i class="bi bi-gear" /> Manage Projects</button
+		class="btn-sm btn-neutral hidden sm:flex"
 	>
+		<i class="bi bi-gear" />
+		<span>Manage Projects</span>
+	</button>
 </SearchBar>
-<div class="m-2 grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+
+<div class="m-4 grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 	{#each $searchQuery ? searchedProjects : data.projects as project, index}
 		<div in:fly={{ delay: (index + 1) * 100, y: 10 }}>
-			<ProjectCard {editModeOn} {project} />
+			<ProjectCard {project} />
 		</div>
 	{/each}
 </div>
