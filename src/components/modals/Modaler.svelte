51a764208a2fbe7cdfed7bc3fb401b8792f802cf
Merkie <merkie@post.com>
2024-01-21 13:45:51 -0600

    Add Modaler component with CreateProjectModal and ManageProjectsModal

---
+++ src/components/modals/Modaler.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { openModal } from '$ts/client/stores';
+
+	import CreateProjectModal from './CreateProjectModal.svelte';
+	import ManageProjectsModal from './ManageProjectsModal.svelte';
+</script>
+
+{#if $openModal.name}
+	<button
+		transition:fade={{ duration: 100 }}
+		on:click={(e) => {
+			$openModal = { name: '' };
+		}}
+		class="fixed left-0 top-0 z-30 grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.7)]"
+	>
+	</button>
+{/if}
+
+<CreateProjectModal />
+<ManageProjectsModal />
