7789acad27c18bc47f5f35ad7cd38809b11087eb
Merkie <merkie@post.com>
2024-01-21 13:01:42 -0600

    improve middleware logic

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,50 +1,36 @@
 import prisma from '$ts/server/prisma';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '$env/static/private';
-import type { User } from '@prisma/client';
 import { json, redirect } from '@sveltejs/kit';
 
 export async function handle({ event, resolve }) {
-	const cookie = event.cookies.get('token');
-
 	// Add prisma to the locals
 	event.locals.prisma = prisma;
 
+	const tokenCookie = event.cookies.get('token');
+
 	// Decode the token and add user to locals
-	if (cookie) {
-		let decodedToken: { id: string } | undefined;
-
+	if (tokenCookie) {
 		try {
-			decodedToken = jwt.verify(cookie, JWT_SECRET) as { id: string };
-		} catch {
-			decodedToken = undefined;
-		}
+			const decoded = jwt.verify(tokenCookie, JWT_SECRET) as { id: string };
+			if (!decoded.id) throw new Error('Invalid token');
 
-		// If the token is valid, add the user to the locals
-		if (decodedToken !== undefined) {
-			const fetchedUser = await prisma.user.findUnique({ where: { id: decodedToken.id } });
-			if (fetchedUser) {
-				delete (fetchedUser as { password?: string }).password;
-				event.locals.user = fetchedUser as Omit<User, 'password'>;
-			}
-		}
+			const fetchedUser = await prisma.user.findUnique({ where: { id: decoded.id } });
+			if (!fetchedUser) throw new Error('User not found');
+
+			event.locals.user = fetchedUser;
+			// eslint-disable-next-line no-empty
+		} catch {}
 	}
 
 	const pathname = event.url.pathname.toLowerCase();
 
 	// API protection
-	if (
-		pathname.startsWith('/api/v1') &&
-		!pathname.startsWith('/api/v1/auth') &&
-		!event.locals.user
-	) {
-		return json({ error: 'Unauthorized request, please log in or create an account.' });
-	}
+	if (pathname.startsWith('/api/v1') && !pathname.startsWith('/api/v1/auth') && !event.locals.user)
+		return json({ error: 'Unauthorized request' }, { status: 401 });
 
 	// App protection
-	if (pathname.startsWith('/app') && !event.locals.user) {
-		throw redirect(307, '/login');
-	} else {
-		return await resolve(event);
-	}
+	if (pathname.startsWith('/app') && !event.locals.user) throw redirect(307, '/login');
+
+	return await resolve(event);
 }
