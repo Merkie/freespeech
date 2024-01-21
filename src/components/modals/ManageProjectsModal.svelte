3686cc5d465443fcef183431220bf6ae9e2d1402
Merkie <merkie@post.com>
2024-01-21 13:45:46 -0600

    Refactor ManageProjectsModal.svelte component

--- src/components/modals/ManageProjectsModal.svelte
+++ src/components/modals/ManageProjectsModal.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
 	import { invalidateAll } from '$app/navigation';
-	import { ActiveProject, openModal } from '$ts/client/stores';
+	import { Loading, openModal } from '$ts/client/stores';
 	import ModalShell from './ModalShell.svelte';
 
-	const deleteProject = async (pageId: string) => {
-		const responseJson = await fetch(`/api/v1/project/${pageId}/delete`, {
+	async function deleteProject(pageId: string) {
+		$Loading = true;
+
+		await fetch(`/api/v1/project/${pageId}/delete`, {
 			method: 'DELETE'
 		}).then((res) => res.json());
 
-		if (responseJson.error) {
-			return alert(responseJson.error);
-		}
+		$Loading = false;
 
 		await invalidateAll();
-	};
+	}
 </script>
 
-<ModalShell title="Manage Projects">
-	{#each $openModal.props.projects || [] as project, index}
+<ModalShell title="Manage Projects" name="manage-projects">
+	{#each $openModal.props?.projects || [] as project, index}
 		<div
 			class={`flex items-center gap-2 py-2 ${
 				index !== 0 ? 'border border-x-0 border-b-0 border-zinc-700' : ''
@@ -25,16 +25,17 @@
 		>
 			<p>{project.name}</p>
 			<div class="flex-1" />
-
 			<button
+				disabled={$Loading}
 				on:click={() => {
 					$openModal = { name: 'edit-project', props: { project } };
 				}}
-				class="rounded-md border border-yellow-500 bg-yellow-600 p-1 px-2 text-sm">Edit</button
+				class="btn-sm btn-warning">Edit</button
 			>
 			<button
+				disabled={$Loading}
 				on:click={() => deleteProject(project.id)}
-				class="rounded-md border border-red-500 bg-red-600 p-1 px-2 text-sm">Delete</button
+				class="btn-sm btn-danger">Delete</button
 			>
 		</div>
 	{/each}
