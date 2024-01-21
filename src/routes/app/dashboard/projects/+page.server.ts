9cf7c32bcd33c8445941f619344b7b5ec805a77d
Merkie <merkie@post.com>
2024-01-21 13:01:55 -0600

    remove uneeded redirect

--- src/routes/app/dashboard/projects/+page.server.ts
+++ src/routes/app/dashboard/projects/+page.server.ts
@@ -1,8 +1,4 @@
-import { redirect } from '@sveltejs/kit';
-
 export const load = async ({ locals }) => {
-	if (!locals.user) throw redirect(302, '/login');
-
 	// get all user projects, sort by most recent
 	const projects = await locals.prisma.project.findMany({
 		where: {
