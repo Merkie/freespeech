eec48134a18b985f76886c8e87b027a7f3c9712e
Merkie <merkie@post.com>
2024-02-06 09:27:19 -0600

    refactor bottom ui

--- src/components/common/BottomNavigation.svelte
+++ src/components/common/BottomNavigation.svelte
@@ -10,49 +10,48 @@
 <div
 	class="flex gap-2 border border-x-0 border-b-0 border-zinc-700 bg-zinc-900 p-2 text-[25px] font-light text-zinc-100"
 >
+	<!-- Home button -->
+	<a
+		class={`${
+			$page.url.pathname.startsWith('/app/') && !$page.url.pathname.startsWith('/app/dashboard')
+				? 'bg-zinc-800'
+				: ''
+		} ${$isEditing ? 'pointer-events-none opacity-50' : ''}`}
+		href={noProjects ? '/app/dashboard' : `/app/project/${projectId}`}
+	>
+		<i class="bi bi-house-fill"></i>
+	</a>
+	<!-- Edit Button -->
 	{#if $isEditing}
-		<!-- Cancel Edits Button -->
+		<!-- Editing State -->
 		<button
-			on:click={async () => {
-				$isEditing = false;
-				$TileBeingEdited = null;
-			}}>Cancel</button
-		>
-		<!-- Save Edits Button -->
-		<button
-			on:click={async () => {
+			on:click={() => {
 				$isEditing = false;
 				$TileBeingEdited = null;
 			}}
-			class="border border-blue-700 bg-blue-600 text-blue-50">Save Changes</button
+			disabled={$page.url.pathname.startsWith('/app/dashboard')}
+			class="flex items-center justify-center gap-4 border border-zinc-700 bg-zinc-800 text-lg"
+		>
+			<i class="bi bi-check-lg"></i><span>Finish Editing</span></button
 		>
 	{:else}
-		<!-- Home button -->
-		<a
-			class={`${
-				$page.url.pathname.startsWith('/app/') && !$page.url.pathname.startsWith('/app/dashboard')
-					? 'bg-zinc-800'
-					: ''
-			}`}
-			href={noProjects ? '/app/dashboard' : `/app/project/${projectId}`}
-		>
-			<i class="bi bi-house-fill"></i>
-		</a>
-		<!-- Edit Button -->
+		<!-- Normal State -->
 		<button
 			on:click={() => {
 				$isEditing = true;
 			}}
-			disabled={noProjects || $page.url.pathname.startsWith('/app/dashboard')}
+			disabled={$page.url.pathname.startsWith('/app/dashboard')}
 			><i class="bi bi-pencil-fill"></i></button
 		>
-		<!-- Dashboard Button -->
-		<a
-			href="/app/dashboard/projects"
-			class={$page.url.pathname.startsWith('/app/dashboard') ? 'bg-zinc-800' : ''}
-			><i class="bi bi-gear-fill"></i></a
-		>
 	{/if}
+
+	<!-- Dashboard Button -->
+	<a
+		href="/app/dashboard/projects"
+		class={`${$page.url.pathname.startsWith('/app/dashboard') ? 'bg-zinc-800' : ''} ${
+			$isEditing ? 'pointer-events-none opacity-50' : ''
+		}`}><i class="bi bi-gear-fill"></i></a
+	>
 </div>
 
 <style lang="postcss">
