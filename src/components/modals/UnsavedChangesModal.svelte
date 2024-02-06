8d2b000eedd209a7435ad2394b975fe39f3dc615
Merkie <merkie@post.com>
2024-02-06 15:48:13 -0600

    Add UnsavedChangesModal component

---
+++ src/components/modals/UnsavedChangesModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { UnsavedChangesHandler, UnsavedChangesModalOpen } from '$ts/client/stores';
+	import ModalShell from './ModalShell.svelte';
+</script>
+
+{#if $UnsavedChangesModalOpen}
+	<ModalShell closeModal={() => ($UnsavedChangesModalOpen = false)} title="Create Project">
+		<p class="mb-2">Project name</p>
+		<div class="mt-2 flex gap-4">
+			<button
+				on:click={() => {
+					$UnsavedChangesModalOpen = false;
+				}}
+				type="submit"
+				class="flex-1 p-2 text-blue-50">Nevermind</button
+			>
+			<button
+				on:click={() => {
+					$UnsavedChangesHandler();
+					$UnsavedChangesModalOpen = false;
+					$UnsavedChangesHandler = () => {};
+				}}
+				type="submit"
+				class="flex-1 rounded-md border border-red-500 bg-red-600 p-2 text-white"
+				>Discard Changes</button
+			>
+		</div>
+	</ModalShell>
+{/if}
