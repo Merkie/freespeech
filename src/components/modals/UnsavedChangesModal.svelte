b55cc227f1f167c86d406083188cc0f7bfd37716
Merkie <merkie@post.com>
2024-02-06 16:15:41 -0600

    add modal text

--- src/components/modals/UnsavedChangesModal.svelte
+++ src/components/modals/UnsavedChangesModal.svelte
@@ -4,8 +4,8 @@
 </script>
 
 {#if $UnsavedChangesModalOpen}
-	<ModalShell closeModal={() => ($UnsavedChangesModalOpen = false)} title="Create Project">
-		<p class="mb-2">Project name</p>
+	<ModalShell closeModal={() => ($UnsavedChangesModalOpen = false)} title="Unsaved Changes">
+		<p class="mb-2">Warning! You have unsaved changes.</p>
 		<div class="mt-2 flex gap-4">
 			<button
 				on:click={() => {
