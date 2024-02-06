80ffd5fb1a6f12d5b0c8e082632aac74dafa9584
Merkie <merkie@post.com>
2024-02-05 19:59:39 -0600

    remove more ai voice stuff

--- src/routes/app/project/[projectId]/[pageId]/+page.server.ts
+++ src/routes/app/project/[projectId]/[pageId]/+page.server.ts
@@ -1,8 +1,6 @@
-import { ELEVEN_LABS_KEY, SITE_SECRET } from '$env/static/private';
 import { redirect } from '@sveltejs/kit';
-import Cryptr from 'cryptr';
 
-export const load = async ({ fetch, locals: { prisma, user }, params: { projectId, pageId } }) => {
+export const load = async ({ locals: { prisma, user }, params: { projectId, pageId } }) => {
 	const page = await prisma.tilePage.findFirst({
 		where: {
 			id: pageId,
@@ -16,19 +14,7 @@ export const load = async ({ fetch, locals: { prisma, user }, params: { projectI
 
 	if (!page) throw redirect(302, '/app/dashboard/projects');
 
-	const voicesData = await fetch('/api/v1/text-to-speech/elevenlabs/voices').then((res) =>
-		res.json()
-	);
-
-	let elevenLabsApiKey = ELEVEN_LABS_KEY;
-	if (user.elevenLabsApiKey) {
-		const cryptr = new Cryptr(SITE_SECRET);
-		elevenLabsApiKey = cryptr.decrypt(user.elevenLabsApiKey);
-	}
-
 	return {
-		page,
-		elevenLabsVoices: voicesData,
-		elevenLabsApiKey
+		page
 	};
 };
