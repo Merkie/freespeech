bf9aaec685924f50c0c432d85df623bd1c6d0ab0
Merkie <merkie@post.com>
2024-02-06 12:33:21 -0600

    add log statement

--- src/routes/api/v1/project/[projectid]/delete/+server.ts
+++ src/routes/api/v1/project/[projectid]/delete/+server.ts
@@ -1,6 +1,7 @@
 import { json } from '@sveltejs/kit';
 
-export const DELETE = async ({ params: { projectId }, locals: { user, prisma } }) => {
+export const DELETE = async ({ params: { projectId }, locals: { user, prisma }, url }) => {
+	console.log(url.pathname, url.pathname.split('project/')[1].split('/delete')[0]);
 	await prisma.project.delete({
 		where: {
 			id: projectId,
