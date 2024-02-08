97f334f03e32384384aac3989cb869c5be2f46f1
Merkie <merkie@post.com>
2024-02-08 13:31:00 -0600

    make text size bigger

--- src/components/app/PageHeader.svelte
+++ src/components/app/PageHeader.svelte
@@ -13,27 +13,27 @@
 	} from '$ts/client/stores';
 </script>
 
-<div class="relative h-12 bg-zinc-900 p-2 font-light text-zinc-100">
+<div class="relative h-14 bg-zinc-900 p-2 font-light text-zinc-100">
 	<p class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">{pageName}</p>
 
 	{#if $EditingTiles}
-		<div class="absolute left-2 top-1/2 flex -translate-y-1/2 items-center gap-2">
+		<div class="absolute left-3 top-1/2 flex -translate-y-1/2 items-center gap-2">
 			<button
 				on:click={() => ($AddingPage = true)}
-				class=" flex items-center gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 px-4 text-sm"
+				class=" flex items-center gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 px-4"
 			>
 				<i class="bi bi-plus-lg"></i>
 				<span>Add New Page</span>
 			</button>
 			<button
 				on:click={() => ($EditingPages = true)}
-				class=" flex items-center gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 px-4 text-sm"
+				class="flex items-center gap-2 rounded-md border border-zinc-700 bg-zinc-800 p-1 px-4"
 			>
 				<i class="bi bi-pencil"></i>
 				<span>Edit Pages</span>
 			</button>
 		</div>
-		<div class="absolute right-2 top-1/2 flex -translate-y-1/2 items-center gap-2">
+		<div class="absolute right-3 top-1/2 flex -translate-y-1/2 items-center gap-2">
 			<button
 				on:click={() => {
 					if (!$UnsavedChanges) {
@@ -51,7 +51,7 @@
 						$UnsavedChangesModalOpen = true;
 					}
 				}}
-				class={`flex items-center gap-2 rounded-md border p-1 px-4 text-sm ${$UnsavedChanges ? 'border-yellow-500 bg-yellow-600' : 'border-blue-500 bg-blue-600'}`}
+				class={`flex items-center gap-2 rounded-md border p-1 px-4  ${$UnsavedChanges ? 'border-yellow-500 bg-yellow-600' : 'border-blue-500 bg-blue-600'}`}
 			>
 				<i class="bi bi-check-lg"></i>
 				<span>{`Finish Editing ${$UnsavedChanges ? '(Unsaved Changes)' : ''}`}</span>
