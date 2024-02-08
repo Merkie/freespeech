20fb48c8f3a3bfabe136aba8c1226a4460c2d009
Merkie <merkie@post.com>
2024-02-07 22:29:56 -0600

    Update ProjectCard styling

--- src/components/dashboard/ProjectCard.svelte
+++ src/components/dashboard/ProjectCard.svelte
@@ -11,15 +11,15 @@
 <div class={`relative p-4`}>
 	<a
 		href={`/app/project/${project.id}`}
-		class={`relative flex h-full w-full flex-col gap-2 rounded-md border border-zinc-300 bg-zinc-200 p-2 ${selected ? 'ring-4 ring-blue-200 ring-offset-4 ring-offset-zinc-100' : ''}`}
+		class={`relative flex h-fit w-full flex-col gap-4  rounded-md border border-zinc-300 bg-zinc-200 p-2 ${selected ? 'ring-4 ring-blue-200 ring-offset-4 ring-offset-zinc-100' : ''}`}
 	>
 		<img
 			src={`${getContext('media_uri')}${project.imageUrl}`}
-			class="aspect-video h-full w-full rounded-sm border border-zinc-300 bg-zinc-300 object-cover text-zinc-300"
+			class="aspect-video w-full rounded-sm border border-zinc-300 bg-zinc-300 object-cover text-zinc-300"
 			alt="preview"
 			width={100}
 		/>
-		<p class="w-full text-left text-lg">
+		<p class="w-full overflow-hidden text-ellipsis whitespace-nowrap text-left text-lg">
 			{project.name}
 		</p>
 	</a>
