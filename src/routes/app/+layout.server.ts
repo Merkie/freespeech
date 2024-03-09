ad2d3e0e776cd8162a689da94280bce51e2d7834
merkie <merkie@post.com>
2024-03-09 16:42:28 -0600

    add elevenlabs endpoint onto base layout

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -1,9 +1,11 @@
 import { redirect } from '@sveltejs/kit';
+import { ELEVEN_LABS_ENDPOINT } from '$env/static/private';
 
 export const load = async ({ locals, params: { projectId } }) => {
 	if (!locals.user) throw redirect(302, '/login');
 
 	return {
-		projectId
+		projectId,
+		ELEVEN_LABS_ENDPOINT
 	};
 };
