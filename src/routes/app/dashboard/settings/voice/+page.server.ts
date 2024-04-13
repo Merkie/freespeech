3f32d153e0a0f11e12cb76e2c66b7c4b6ffd4a9e
merkie <merkie@post.com>
2024-04-13 12:41:43 -0500

    add bad key handling

--- src/routes/app/dashboard/settings/voice/+page.server.ts
+++ src/routes/app/dashboard/settings/voice/+page.server.ts
@@ -5,11 +5,22 @@ import { DecryptElevenLabsKey } from '$ts/server/decrypt-key';
 export const load = async ({ locals: { user } }) => {
 	const userKey = DecryptElevenLabsKey(user.elevenLabsApiKey);
 
+	if (user.usePersonalElevenLabsKey && !userKey)
+		return {
+			voices: [],
+			elevenLabsApiKey: user.elevenLabsApiKey,
+			usePersonalElevenLabsKey: user.usePersonalElevenLabsKey
+		};
+
 	const { voices } = await fetch('https://api.elevenlabs.io/v1/voices', {
 		headers: {
 			'xi-api-key': userKey || ELEVEN_LABS_KEY
 		}
 	}).then((res) => res.json());
 
-	return { voices, elevenLabsApiKey: userKey };
+	return {
+		voices,
+		elevenLabsApiKey: userKey,
+		usePersonalElevenLabsKey: user.usePersonalElevenLabsKey
+	};
 };
