915bc5343543d6b6dee1c569dcc892350a4d5e2f
merkie <merkie@post.com>
2024-04-13 13:42:04 -0500

    add settings page ui

--- src/routes/app/dashboard/settings/+page.svelte
+++ src/routes/app/dashboard/settings/+page.svelte
@@ -1,13 +1,94 @@
-<div class="grid h-full flex-1 grid-cols-4 grid-rows-3 gap-8 p-8">
-	<a
-		href="/app/dashboard/settings/voice"
-		class="flex h-full flex-col rounded-xl border border-blue-500 bg-blue-100 p-8"
-	>
-		<p class="text-4xl font-semibold text-zinc-800">Voice</p>
-		<div class="flex-1"></div>
-		<p>
-			Change the voice used by the app. You can use online AI-powered voices or offline browser
-			voices. AI voices are from ElevenLabs and are more natural-sounding.
-		</p>
-	</a>
+<div class="flex flex-col gap-8 p-8 pb-6 pt-14">
+	<p class="text-5xl text-zinc-800">Application Settings</p>
+</div>
+
+<div class="flex flex-col gap-8 p-8 pb-[200px]">
+	<div class="flex flex-col gap-8 md:flex-row">
+		<a
+			href="/app/dashboard/profile"
+			class="group flex flex-1 items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 transition-all hover:border-pink-200 hover:bg-pink-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-pink-100">
+				<i class="bi bi-person-circle text-[40px] text-pink-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-pink-500">Account</span>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-pink-500"
+			></i>
+		</a>
+		<a
+			href="/app/dashboard/settings/voice"
+			class="group flex flex-1 select-none items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 transition-all hover:border-blue-200 hover:bg-blue-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-blue-100">
+				<i class="bi bi-volume-up-fill text-[40px] text-blue-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-blue-500">Voice</span>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-blue-500"
+			></i>
+		</a>
+	</div>
+	<div class="flex flex-col gap-8 md:flex-row">
+		<a
+			href="/app/dashboard/settings/"
+			class="group pointer-events-none flex flex-1 select-none items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all hover:border-orange-200 hover:bg-orange-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-orange-100">
+				<i class="bi bi-people-fill text-[40px] text-orange-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-orange-500">Sharing</span>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-orange-500"
+			></i>
+		</a>
+		<a
+			href="/app/dashboard/settings/"
+			class="group pointer-events-none flex flex-1 select-none items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all hover:border-green-200 hover:bg-green-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-green-100">
+				<i class="bi bi-bar-chart-fill text-[40px] text-green-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-green-500">Usage</span>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-green-500"
+			></i>
+		</a>
+	</div>
+	<div class="flex flex-col gap-8 md:flex-row">
+		<a
+			href="/app/dashboard/settings/"
+			class="group pointer-events-none flex flex-1 select-none items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all hover:border-amber-200 hover:bg-amber-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-amber-100">
+				<i class="bi bi-lock-fill text-[40px] text-amber-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-amber-500"
+				>Access Controls</span
+			>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-amber-500"
+			></i>
+		</a>
+		<a
+			href="/app/dashboard/settings/"
+			class="group pointer-events-none flex flex-1 select-none items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all hover:border-purple-200 hover:bg-purple-50"
+		>
+			<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-purple-100">
+				<i class="bi bi-palette-fill text-[40px] text-purple-500"></i>
+			</div>
+			<span class="text-3xl text-zinc-800 transition-all group-hover:text-purple-500"
+				>Appearance</span
+			>
+			<div class="flex-1"></div>
+			<i
+				class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-purple-500"
+			></i>
+		</a>
+	</div>
 </div>
