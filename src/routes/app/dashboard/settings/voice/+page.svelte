5074baa5b13eea527d3928371c67838602843a94
merkie <merkie@post.com>
2024-04-13 11:18:26 -0500

    add select voice label

--- src/routes/app/dashboard/settings/voice/+page.svelte
+++ src/routes/app/dashboard/settings/voice/+page.svelte
@@ -21,13 +21,16 @@
 		</button>
 	</div>
 
-	<div
-		class={`grid gap-8 transition-all ${$EnableThirdPartyVoiceProviders ? 'grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1' : 'grid-cols-1 grid-rows-1'}`}
-	>
-		{#if $EnableThirdPartyVoiceProviders}
-			<ElevenLabsVoiceSelector voices={data.voices} />
-		{/if}
-		<InternalVoiceSelector />
+	<div class="flex flex-col gap-4">
+		<p class="text-3xl text-zinc-800">Select voice:</p>
+		<div
+			class={`grid gap-8 transition-all ${$EnableThirdPartyVoiceProviders ? 'grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1' : 'grid-cols-1 grid-rows-1'}`}
+		>
+			{#if $EnableThirdPartyVoiceProviders}
+				<ElevenLabsVoiceSelector voices={data.voices} />
+			{/if}
+			<InternalVoiceSelector />
+		</div>
 	</div>
 
 	<TestVoice />
