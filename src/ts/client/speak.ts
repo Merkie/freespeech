5ab6dfc8e82576244929bdba9094e690bebfa34c
merkie <merkie@post.com>
2024-04-13 13:08:41 -0500

    add speech synth to speak function

--- src/ts/client/speak.ts
+++ src/ts/client/speak.ts
@@ -1,9 +1,16 @@
 import { Howl } from 'howler';
-import { ElevenLabsVoiceId, VoiceEngineStatus } from './stores';
+import {
+	ElevenLabsVoiceId,
+	EnableThirdPartyVoiceProviders,
+	OfflineVoiceUri,
+	VoiceEngineStatus
+} from './stores';
 import { get } from 'svelte/store';
 
 export async function speakText(text: string) {
 	VoiceEngineStatus.set('synthesizing');
+
+	if (!get(EnableThirdPartyVoiceProviders) || !get(ElevenLabsVoiceId)) return speakSynth(text);
 
 	const response = await fetch('/api/v1/text-to-speech/elevenlabs/speak', {
 		method: 'POST',
@@ -30,4 +37,25 @@ export async function speakText(text: string) {
 	sound.on('end', () => {
 		VoiceEngineStatus.set('ready');
 	});
+
+	sound.on('loaderror', () => {
+		speakSynth(text);
+	});
+}
+
+function speakSynth(text: string) {
+	const synthVoiceUri = get(OfflineVoiceUri);
+
+	// make speech synthesis request
+	const synth = new SpeechSynthesisUtterance(text);
+	const voice = speechSynthesis.getVoices().find((voice) => voice.voiceURI === synthVoiceUri);
+	if (voice) {
+		synth.voice = voice;
+	}
+
+	speechSynthesis.speak(synth);
+
+	synth.onend = () => {
+		VoiceEngineStatus.set('ready');
+	};
 }
