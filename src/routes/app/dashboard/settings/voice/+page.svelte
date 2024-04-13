1667de0220c40ece9d501af7042400bd3f5883c7
merkie <merkie@post.com>
2024-04-13 11:23:53 -0500

    add back button and settings page header

--- src/routes/app/dashboard/settings/voice/+page.svelte
+++ src/routes/app/dashboard/settings/voice/+page.svelte
@@ -8,6 +8,18 @@
 </script>
 
 <div class="flex flex-col gap-12 p-8">
+	<div class="flex flex-col gap-8">
+		<a
+			href="/app/dashboard/settings"
+			class="w-fit p-2 pl-0 text-xl text-zinc-600 hover:text-zinc-800"
+		>
+			<i class="bi bi-arrow-left-short"></i>
+			<span>Back</span>
+		</a>
+
+		<p class="border-b border-zinc-300 pb-8 text-4xl font-semibold text-zinc-600">Voice Settings</p>
+	</div>
+
 	<div class="flex items-center gap-4">
 		<p class="text-3xl text-zinc-800">Enable third-party voice providers:</p>
 		<button
