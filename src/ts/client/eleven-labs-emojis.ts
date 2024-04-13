74594652b7e775c6d401773fcc2c8c33d2109c0b
merkie <merkie@post.com>
2024-04-13 11:13:04 -0500

    add eleven labs emojis to static file

---
+++ src/ts/client/eleven-labs-emojis.ts
@@ -0,0 +1,54 @@
+export const emojis = {
+	accent: {
+		american: '🇺🇸',
+		'british-essex': '🇬🇧💅',
+		irish: '🇮🇪',
+		australian: '🇦🇺',
+		british: '🇬🇧',
+		'english-swedish': '🇸🇪🇬🇧',
+		'american-irish': '🇺🇸🇮🇪',
+		'american-southern': '🇺🇸🌵',
+		'english-italian': '🇬🇧🇮🇹',
+		indian: '🇮🇳',
+		african: '🌍'
+	},
+	description: {
+		calm: '🍃',
+		'well-rounded': '🟢',
+		'war veteran': '🪖',
+		'ground reporter': '🎤',
+		strong: '💪',
+		conversational: '💬',
+		sailor: '⚓',
+		soft: '🧸',
+		casual: '👕',
+		raspy: '🗣️',
+		emotional: '💖',
+		hoarse: '😶‍🌫️',
+		shouty: '📢',
+		anxious: '😟',
+		pleasant: '😊',
+		deep: '🌊',
+		crisp: '🍎',
+		seductive: '💋',
+		confident: '😎',
+		warm: '🌞',
+		'calm ': '🍃',
+		excited: '🎉',
+		childlish: '🍼', // Note: Presuming "childlish" is a typo for "childish"
+		whisper: '🤫',
+		'raspy ': '🗣️',
+		witch: '🧙‍♀️',
+		foreigner: '🌏',
+		childish: '🧒'
+	},
+	age: {
+		young: '👶',
+		'middle aged': '🧑‍🦳',
+		old: '👴'
+	},
+	gender: {
+		female: '♀️',
+		male: '♂️'
+	}
+};
