34a7bc26fa0e9236fadc9ec69c32429d2ccba675
merkie <merkie@post.com>
2024-04-13 12:06:11 -0500

    fetch personal key on dashboard

--- src/routes/app/dashboard/settings/voice/+page.server.ts
+++ src/routes/app/dashboard/settings/voice/+page.server.ts
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ELEVEN_LABS_KEY } from '$env/static/private';
+import { ELEVEN_LABS_KEY, SITE_SECRET } from '$env/static/private';
+import Cryptr from 'cryptr';
 
-export const load = async () => {
+export const load = async ({ locals: { user } }) => {
 	const { voices } = await fetch('https://api.elevenlabs.io/v1/voices', {
 		headers: {
 			'xi-api-key': ELEVEN_LABS_KEY
 		}
 	}).then((res) => res.json());
 
-	return { voices };
+	let elevenLabsApiKey = '';
+
+	if (user.elevenLabsApiKey) {
+		const cryptr = new Cryptr(SITE_SECRET);
+		elevenLabsApiKey = cryptr.decrypt(user.elevenLabsApiKey);
+	}
+
+	return { voices, elevenLabsApiKey };
 };
