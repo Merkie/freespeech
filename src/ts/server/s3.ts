56e180b6dbbde3f603b3bd51813120a0efd85219
merkie <merkie@post.com>
2024-04-14 16:47:04 -0500

    Update S3Client configuration in s3.ts

--- src/ts/server/s3.ts
+++ src/ts/server/s3.ts
@@ -3,6 +3,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 const s3 = new S3Client({
 	endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+	region: 'auto',
 	credentials: {
 		accessKeyId: `${R2_ACCESS_KEY}`,
 		secretAccessKey: `${R2_SECRET_KEY}`
