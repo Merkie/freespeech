fd3719b7e1b00c47170d48fafeeb6cd8bb4a9b0d
merkie <merkie@post.com>
2024-06-16 19:00:30 -0500

    restyle modal shell

--- src/components/modals/ModalShell.svelte
+++ src/components/modals/ModalShell.svelte
@@ -27,19 +27,19 @@
 				closeModal();
 			}
 		}}
-		class="fixed left-0 top-0 z-30 grid h-screen w-screen place-items-center bg-[rgba(0,0,0,0.7)]"
+		class="fixed left-0 top-0 z-30 grid h-screen w-screen place-items-center bg-black/[75%]"
 	>
 		<div
 			transition:fly={{ y: 100, duration: 200 }}
-			class="flex max-h-[90%] w-[100%] flex-col overflow-y-auto border border-zinc-800 bg-zinc-900 p-2 text-zinc-200 shadow-md sm:w-[90%] sm:max-w-[500px] sm:rounded-md"
+			class="flex max-h-[90%] w-[100%] flex-col overflow-y-auto border border-zinc-800 bg-zinc-900 p-6 text-zinc-200 shadow-md sm:w-[90%] sm:max-w-[500px] sm:rounded-xl"
 		>
-			<div class="mb-2 flex items-center gap-2">
+			<div class="mb-2 flex items-center justify-between gap-2">
+				<p class="text-lg font-bold">{title}</p>
 				<button on:click={closeModal}>
-					<i class="bi bi-x" />
+					<i class="bi bi-x-lg flex items-center" />
 				</button>
-				<p>{title}</p>
 			</div>
-			<div class="flex flex-col p-2">
+			<div class="flex flex-col">
 				<slot />
 			</div>
 		</div>
