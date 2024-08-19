f94c36e173f1e09c51d023c85c49f3f9080222d2
merkie <merkie@post.com>
2024-08-19 16:24:10 -0500

    add user and tts endpoints

--- src/ts/client/api/index.ts
+++ src/ts/client/api/index.ts
@@ -1,9 +1,13 @@
 import auth from './endpoints/auth';
 import media from './endpoints/media';
+import tts from './endpoints/tts';
+import user from './endpoints/user';
 
 const api = {
 	auth,
-	media
+	media,
+	user,
+	tts
 };
 
 export default api;
