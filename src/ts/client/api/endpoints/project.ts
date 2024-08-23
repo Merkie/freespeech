b9b97fbd92388e729d726612ae88b486fac1b653
merkie <merkie@post.com>
2024-08-23 13:59:10 -0500

    add project endpoint file

---
+++ src/ts/client/api/endpoints/project.ts
@@ -0,0 +1,47 @@
+import { PUBLIC_API_URL } from '$env/static/public';
+import type { Project } from '$ts/common/types';
+
+const project = {
+	list: listProjects,
+	create: createProject
+};
+
+export default project;
+
+async function listProjects(token?: string) {
+	const response = await fetch(PUBLIC_API_URL + '/project/list', {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token ? token : localStorage.getItem('token')}`
+		}
+	});
+	const data = (await response.json()) as {
+		projects: Project[];
+	};
+	return data;
+}
+
+async function createProject({
+	name,
+	columns,
+	rows
+}: {
+	name: string;
+	columns: number;
+	rows: number;
+}) {
+	const response = await fetch(PUBLIC_API_URL + '/project/create', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ name, columns, rows, isPublic: false, description: '' })
+	});
+
+	const data = (await response.json()) as {
+		success: boolean;
+		error: string;
+	};
+
+	return data;
+}
