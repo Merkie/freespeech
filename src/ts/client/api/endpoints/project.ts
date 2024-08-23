91bc445484db8c2e6f36b65cce924a26c616c54f
merkie <merkie@post.com>
2024-08-23 14:11:55 -0500

    add delete project handler

--- src/ts/client/api/endpoints/project.ts
+++ src/ts/client/api/endpoints/project.ts
@@ -3,7 +3,8 @@ import type { Project } from '$ts/common/types';
 
 const project = {
 	list: listProjects,
-	create: createProject
+	create: createProject,
+	delete: deleteProject
 };
 
 export default project;
@@ -36,6 +37,23 @@ async function createProject({
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({ name, columns, rows, isPublic: false, description: '' })
+	});
+
+	const data = (await response.json()) as {
+		success: boolean;
+		error: string;
+	};
+
+	return data;
+}
+
+async function deleteProject(projectId: string) {
+	const response = await fetch(`${PUBLIC_API_URL}/project/${projectId}/delete`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('token')}`
+		}
 	});
 
 	const data = (await response.json()) as {
