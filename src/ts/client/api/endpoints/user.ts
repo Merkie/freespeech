641dbfb39f82d12249b041898fe524c8ff236e48
merkie <merkie@post.com>
2024-08-23 16:47:24 -0500

    add get key handler

--- src/ts/client/api/endpoints/user.ts
+++ src/ts/client/api/endpoints/user.ts
@@ -1,7 +1,8 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 
 const user = {
-	update: updateUser
+	update: updateUser,
+	getElevenLabsKey
 };
 
 export default user;
@@ -23,6 +24,22 @@ async function updateUser(body: {
 
 	const json = (await response.json()) as {
 		success: boolean;
+	};
+
+	return json;
+}
+
+async function getElevenLabsKey(token?: string) {
+	const response = await fetch(PUBLIC_API_URL + '/user/eleven-labs-key', {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token ? token : localStorage.getItem('token')}`
+		}
+	});
+
+	const json = (await response.json()) as {
+		key: string;
+		error: string;
 	};
 
 	return json;
