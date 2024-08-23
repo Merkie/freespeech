a562bd51d44dd530a1a977bacb9d9c792526b883
merkie <merkie@post.com>
2024-08-23 16:47:17 -0500

    add get voices handler

--- src/ts/client/api/endpoints/tts.ts
+++ src/ts/client/api/endpoints/tts.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PUBLIC_API_URL } from '$env/static/public';
 import { ElevenLabsVoiceId } from '$ts/client/stores';
 import { get } from 'svelte/store';
@@ -5,6 +6,9 @@ import { get } from 'svelte/store';
 const tts = {
 	speak: {
 		elevenlabs: ElevenLabsSpeak
+	},
+	voices: {
+		elevenlabs: ElevenLabsVoices
 	}
 };
 
@@ -26,4 +30,20 @@ async function ElevenLabsSpeak(text: string) {
 	const data = await response.blob();
 
 	return data;
+}
+
+async function ElevenLabsVoices(token?: string) {
+	const response = await fetch(PUBLIC_API_URL + '/text-to-speech/elevenlabs/voices', {
+		headers: {
+			Authorization: `Bearer ${token ? token : localStorage.getItem('token')}`,
+			'Content-Type': 'application/json'
+		}
+	});
+
+	const json = (await response.json()) as {
+		voices: any[];
+		error: string;
+	};
+
+	return json;
 }
