627c2216d476bee12ff94c64b9dbe0d4ccb34761
merkie <merkie@post.com>
2024-08-23 16:46:49 -0500

    remove prisma from app types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,10 @@
 import type { User } from '$ts/common/types';
-import type { PrismaClient } from '@prisma/client';
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			user: User;
-			prisma: PrismaClient;
 		}
 		// interface PageData {}
 		// interface Platform {}
