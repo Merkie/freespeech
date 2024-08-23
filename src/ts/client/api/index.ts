6e7e5e176b148953c5ddbb474a2b23a670156e7c
merkie <merkie@post.com>
2024-08-23 13:58:57 -0500

    add project endpoint handler file to api

--- src/ts/client/api/index.ts
+++ src/ts/client/api/index.ts
@@ -2,12 +2,14 @@ import auth from './endpoints/auth';
 import media from './endpoints/media';
 import tts from './endpoints/tts';
 import user from './endpoints/user';
+import project from './endpoints/project';
 
 const api = {
 	auth,
 	media,
 	user,
-	tts
+	tts,
+	project
 };
 
 export default api;
