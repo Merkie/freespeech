a64678aaac5e603222b17f21d146ad5dfaab7141
merkie <merkie@post.com>
2024-08-23 13:56:14 -0500

    add api.auth.me instead of prisma call

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,6 @@
 import prisma from '$ts/server/prisma';
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '$env/static/private';
-import type { User } from '@prisma/client';
 import { json, redirect } from '@sveltejs/kit';
+import api from '$ts/client/api';
 
 export async function handle({ event, resolve }) {
 	const cookie = event.cookies.get('token');
@@ -12,21 +10,9 @@ export async function handle({ event, resolve }) {
 
 	// Decode the token and add user to locals
 	if (cookie) {
-		let decodedToken: { id: string } | undefined;
-
-		try {
-			decodedToken = jwt.verify(cookie, JWT_SECRET) as { id: string };
-		} catch {
-			decodedToken = undefined;
-		}
-
-		// If the token is valid, add the user to the locals
-		if (decodedToken !== undefined) {
-			const fetchedUser = await prisma.user.findUnique({ where: { id: decodedToken.id } });
-			if (fetchedUser) {
-				delete (fetchedUser as { password?: string }).password;
-				event.locals.user = fetchedUser as Omit<User, 'password'>;
-			}
+		const meResponse = await api.auth.me(cookie);
+		if (meResponse.user) {
+			event.locals.user = meResponse.user;
 		}
 	}
 
