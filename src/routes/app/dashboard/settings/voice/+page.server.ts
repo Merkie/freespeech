f657ac792517c311395e442be10c92464d458e5b
merkie <merkie@post.com>
2024-08-23 16:47:10 -0500

    use new handlers

--- src/routes/app/dashboard/settings/voice/+page.server.ts
+++ src/routes/app/dashboard/settings/voice/+page.server.ts
@@ -1,47 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ELEVEN_LABS_KEY } from '$env/static/private';
-import { DecryptElevenLabsKey } from '$ts/server/decrypt-key';
+import api from '$ts/client/api';
 
-export const load = async ({ locals: { user } }) => {
-	const userKey = user.usePersonalElevenLabsKey ? DecryptElevenLabsKey(user.elevenLabsApiKey) : '';
-
-	if (user.usePersonalElevenLabsKey && !userKey)
-		return {
-			voices: [],
-			elevenLabsApiKey: user.elevenLabsApiKey,
-			usePersonalElevenLabsKey: user.usePersonalElevenLabsKey
-		};
-
-	let voices = [];
-
-	try {
-		const response = await fetch('https://api.elevenlabs.io/v1/voices', {
-			headers: {
-				'xi-api-key': userKey || ELEVEN_LABS_KEY
-			}
-		}).then((res) => res.json());
-
-		if (response.voices && Array.isArray(response.voices)) voices = response.voices;
-	} catch {
-		// empty
-	}
-
-	voices.sort((a: any, b: any) => {
-		if (a.category === 'premade' && b.category !== 'premade') return 1;
-		if (a.category !== 'premade' && b.category == 'premade') return -1;
-		return a.name.localeCompare(b.name);
-	});
-
-	// filter out the "seductive" voices lol
-	// TODO: add a setting for this
-	const filteredVoices = voices.filter(
-		(voice: any) =>
-			!(voice.category === 'premade' && voice.labels.description?.includes('seductive'))
-	);
+export const load = async ({ locals: { user }, cookies }) => {
+	const [{ voices }, { key: userPersonalELKey }] = await Promise.all([
+		api.tts.voices.elevenlabs(cookies.get('token')),
+		api.user.getElevenLabsKey(cookies.get('token'))
+	]);
 
 	return {
-		voices: filteredVoices,
-		elevenLabsApiKey: userKey,
+		voices: voices,
+		elevenLabsApiKey: userPersonalELKey,
 		usePersonalElevenLabsKey: user.usePersonalElevenLabsKey
 	};
 };
