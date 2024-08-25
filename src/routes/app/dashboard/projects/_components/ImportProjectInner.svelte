6f43ba5ce6e1c26458c2b5ac391b4bb20d8c61f5
merkie <merkie@post.com>
2024-08-25 11:38:25 -0500

    add ImportProjectInner component

---
+++ src/routes/app/dashboard/projects/_components/ImportProjectInner.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import JSZip from 'jszip';
+	import Dropzone from 'svelte-file-dropzone';
+	import type { OBFPage } from '$ts/common/openboardformat';
+	import { invalidateAll } from '$app/navigation';
+	import api from '$ts/client/api';
+
+	let files: {
+		accepted: File[];
+		rejected: File[];
+	} = {
+		accepted: [],
+		rejected: []
+	};
+
+	function handleFilesSelect(e: CustomEvent<File>) {
+		const { acceptedFiles, fileRejections } = e.detail as unknown as {
+			acceptedFiles: File[];
+			fileRejections: File[];
+		};
+		files.accepted = [...files.accepted, ...acceptedFiles];
+		files.rejected = [...files.rejected, ...fileRejections];
+
+		files.accepted.map(async (file) => {
+			if (file.name.endsWith('.obz')) {
+				const zip = new JSZip();
+
+				const obzDirectory = await zip.loadAsync(file);
+				const manifestFile = await obzDirectory.file('manifest.json')?.async('string');
+				if (!manifestFile) return;
+
+				const manifest = JSON.parse(manifestFile);
+				if (!manifest || !manifest.root) return;
+
+				const obfFiles = Object.keys(obzDirectory.files).filter((fileName) =>
+					fileName.endsWith('.obf')
+				);
+
+				const obfFilesWithContent = await Promise.all(
+					obfFiles.map(async (fileName) => {
+						const file = obzDirectory.file(fileName);
+						if (!file) return;
+						const data = await file.async('string');
+						return { fileName, data: JSON.parse(data) as any };
+					})
+				);
+
+				await api.project.import.obz({
+					manifest,
+					obfFiles: obfFilesWithContent
+				});
+
+				await invalidateAll();
+			} else if (file.name.endsWith('.obf')) {
+				const reader = new FileReader();
+				reader.readAsText(file);
+				reader.onload = async function () {
+					const data = reader.result;
+					const obfPage = JSON.parse(data as string) as OBFPage;
+
+					await api.project.import.obf(obfPage);
+
+					await invalidateAll();
+				};
+			}
+		});
+	}
+</script>
+
+<Dropzone accept={['.obz', '.obf']} multiple={false} on:drop={handleFilesSelect} />
+<ol>
+	{#each files.accepted as item}
+		<li>{item.name}</li>
+	{/each}
+</ol>
