6e4b78f50f28d31f6678a0001fd3446aa5757e80
merkie <merkie@post.com>
2024-08-26 17:06:14 -0500

    fix text height with images fix tile hovering

--- src/components/app/Tile.svelte
+++ src/components/app/Tile.svelte
@@ -14,6 +14,8 @@
 
 	export let tile: Tile;
 
+	let tileTextContainerHeight = 0;
+
 	const handleInteraction = () => {
 		if ($EditingTiles) {
 			// deselect tile
@@ -45,28 +47,38 @@
 </script>
 
 <div
-	class={`relative h-full w-full transition-opacity ${$EditingTiles && $TileBeingEdited && $TileBeingEdited.id !== tile.id ? 'opacity-40' : ''}`}
+	class={`group relative h-full w-full transition-opacity ${$EditingTiles && $TileBeingEdited && $TileBeingEdited.id !== tile.id ? 'opacity-40' : ''}`}
 >
 	<button
 		on:click={handleInteraction}
-		style={`background-color: ${tile.backgroundColor}; border-color: ${tile.borderColor};`}
+		style={`background-color: ${tile.backgroundColor}; border-color: ${tile.borderColor};${tile.image ? ' grid-template-rows: 25% 75%;' : ''}`}
 		class={`absolute left-0 top-0 h-full w-full overflow-hidden rounded-md border ${
-			tile.image ? 'flex flex-col items-center' : 'grid place-items-center'
+			tile.image ? 'grid grid-cols-1 grid-rows-2' : 'grid place-items-center'
 		}`}
 	>
-		<p class={`w-full truncate ${!tile.image ? 'text-[2vw]' : 'py-2'}`}>
-			{tile.displayText || tile.text}
-		</p>
 		{#if tile.image}
-			<div class="relative w-full flex-1">
-				<img src={tile.image} class="absolute h-full w-full object-contain" alt="Tile media" />
+			<div bind:clientHeight={tileTextContainerHeight} class="relative h-full w-full">
+				<div class="absolute left-0 top-0 flex h-full w-full items-center justify-center">
+					<p
+						style={`font-size: ${Math.max(3, Math.floor(tileTextContainerHeight * 0.7))}px;`}
+						class="flex-1 truncate px-2 text-center"
+					>
+						{tile.displayText || tile.text}
+					</p>
+				</div>
 			</div>
+
+			<img src={tile.image} class="h-full w-full object-contain" alt="Tile media" />
+		{:else}
+			<p class={`w-full truncate ${!tile.image ? 'text-[2vw]' : 'py-2'}`}>
+				{tile.displayText || tile.text}
+			</p>
 		{/if}
 	</button>
 	{#if tile.navigation}
 		<div
 			style={`background-color: ${tile.backgroundColor}; border-color: ${tile.borderColor};`}
-			class={`absolute -top-1 left-0 h-[10px] w-[50%] rounded-t-md border border-b-0`}
+			class={`absolute -top-1 left-0 h-[10px] w-[50%] rounded-t-md border border-b-0 brightness-100 group-hover:brightness-105 group-active:brightness-100`}
 		/>
 	{/if}
 </div>
