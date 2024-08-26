2e0030147dcbae3f9b2c608ac382beb1bb43e000
merkie <merkie@post.com>
2024-08-26 13:52:35 -0500

    Use env for getting the image url prefix

--- src/components/dashboard/ProjectCard.svelte
+++ src/components/dashboard/ProjectCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+	import { PUBLIC_R2_URL } from '$env/static/public';
 	import { LocalSettings } from '$ts/client/stores';
 	import type { Project } from '$ts/common/types';
-	import { getContext } from 'svelte';
 
 	export let project: Project;
 
@@ -14,7 +14,7 @@
 		class={`relative flex h-fit w-full flex-col gap-4  rounded-md border border-zinc-300 bg-zinc-200 p-2 ${selected ? 'ring-4 ring-blue-200 ring-offset-4 ring-offset-zinc-100' : ''}`}
 	>
 		<img
-			src={`${getContext('media_uri')}${project.imageUrl}`}
+			src={`${PUBLIC_R2_URL}${project.imageUrl}`}
 			class="aspect-video w-full rounded-sm border border-zinc-300 bg-zinc-300 object-cover text-zinc-300"
 			alt="preview"
 			width={100}
