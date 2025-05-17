c1c2b0333dc5c0b7f4440e5c28bbdb2d5607f89b
merkie <merkie@post.com>
2025-05-17 14:59:34 -0500

    Refactor logRequest middleware to use default export and unify string quotes

--- server/src/lib/middlewares/log-request.ts
+++ server/src/lib/middlewares/log-request.ts
@@ -1,6 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
-export function logRequest(req: Request, res: Response, next: NextFunction) {
+export default function logRequest(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   console.log(
     `[${new Date(Date.now()).toUTCString()}] ${req.method} ${req.path}`
   );
