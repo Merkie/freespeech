7bb2575c460d79111e8c60ed810b13af788baf08
merkie <merkie@post.com>
2025-05-24 14:12:42 -0500

    Add project state management with signals for projects and loading state

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,5 +1,10 @@
 import { createSignal } from "solid-js";
-import { User } from "./lib/types";
+import { Project, User } from "./lib/types";
 
 export const [token, setToken] = createSignal("");
 export const [user, setUser] = createSignal<User | null>(null);
+
+export const [projects, setProjects] = createSignal<Project[]>([]);
+export const [projectsLoadingState, setProjectsLoadingState] = createSignal<
+  "unloaded" | "loading" | "loaded"
+>("unloaded");
