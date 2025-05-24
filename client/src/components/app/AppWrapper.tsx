8cd5b7d501bc5c517432f06f52fdb59f90dfd076
merkie <merkie@post.com>
2025-05-24 13:48:40 -0500

    Refactor AppWrapper to include user authentication check and conditional rendering

--- client/src/components/app/AppWrapper.tsx
+++ client/src/components/app/AppWrapper.tsx
@@ -1,12 +1,34 @@
-import { RouteSectionProps } from "@solidjs/router";
-import { Component } from "solid-js";
+import api from "@/lib/api";
+import { setUser, user } from "@/state";
+import { RouteSectionProps, useNavigate } from "@solidjs/router";
+import { Component, onMount, Show } from "solid-js";
 
 const AppWrapper: Component<RouteSectionProps<unknown>> = (props) => {
+  const navigate = useNavigate();
+
+  onMount(async () => {
+    if (user()) return;
+
+    const token = localStorage.getItem("token");
+    if (token) {
+      const data = await api.auth.me();
+      if (data.user) {
+        setUser(data.user);
+      }
+    }
+
+    if (!user()) {
+      navigate("/login");
+    }
+  });
+
   return (
-    <div>
-      <h1>App Wrapper</h1>
-      {props.children}
-    </div>
+    <Show when={user()}>
+      <div>
+        <h1>App Wrapper</h1>
+        {props.children}
+      </div>
+    </Show>
   );
 };
 
