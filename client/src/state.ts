4d020b8435a94dbe0e7b60131683ac746d1fb467
merkie <merkie@post.com>
2025-05-24 13:48:36 -0500

    Add user state management with Solid.js signals

--- client/src/state.ts
+++ client/src/state.ts
@@ -1,3 +1,5 @@
 import { createSignal } from "solid-js";
+import { User } from "./lib/types";
 
 export const [token, setToken] = createSignal("");
+export const [user, setUser] = createSignal<User | null>(null);
