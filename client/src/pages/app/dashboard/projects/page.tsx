0edb109677aa9f9243dc258cf12a6af0db4d45ca
merkie <merkie@post.com>
2025-05-24 14:12:48 -0500

    Fetch and display projects on dashboard load; update state management for projects

--- client/src/pages/app/dashboard/projects/page.tsx
+++ client/src/pages/app/dashboard/projects/page.tsx
@@ -1,6 +1,25 @@
-import { user } from "@/state";
+import api from "@/lib/api";
+import {
+  projects,
+  projectsLoadingState,
+  setProjects,
+  setProjectsLoadingState,
+  user,
+} from "@/state";
+import { onMount } from "solid-js";
 
 export default function Page() {
+  onMount(async () => {
+    if (projectsLoadingState() === "unloaded") {
+      setProjectsLoadingState("loading");
+
+      const { projects } = await api.project.list();
+      if (projects) setProjects(projects);
+
+      setProjectsLoadingState("loaded");
+    }
+  });
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -9,7 +28,9 @@ export default function Page() {
         settings.
       </p>
       <p>Use the navigation menu to explore different sections.</p>
-      <pre>{JSON.stringify({ user: user() }, null, 2)}</pre>
+      <pre>
+        {JSON.stringify({ user: user(), projects: projects() }, null, 2)}
+      </pre>
     </div>
   );
 }
