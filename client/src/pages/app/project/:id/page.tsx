0dcc18013c15831700c3beb55f40895fc08a8fd6
merkie <merkie@post.com>
2025-06-21 18:27:18 -0500

    Refactor Page component to improve project data fetching and tile rendering logic

--- client/src/pages/app/project/:id/page.tsx
+++ client/src/pages/app/project/:id/page.tsx
@@ -1,9 +1,16 @@
 import api from "@/lib/api";
 import { createMemo, createSignal, For, onMount, Show } from "solid-js";
 import { useParams } from "@solidjs/router";
-import { page, project, setLocalSettings, setPage, setProject } from "@/state";
+import {
+  currentPageId,
+  PageExpanded,
+  project,
+  setCurrentPageId,
+  setLocalSettings,
+  setProject,
+  TileExpanded,
+} from "@/state";
 import useMeasure from "@/lib/hooks/useMeasure";
-import { Tile } from "@/lib/types";
 import { cn } from "@/lib/cn";
 export default function Page() {
   const params = useParams();
@@ -12,23 +19,14 @@ export default function Page() {
     // set the last visited project id in local settings
     setLocalSettings("lastVisitedProjectId", params.id);
 
-    // -- data fetching --
-
-    // 1) Fetch the project by ID
-    const { project } = await api.project.view(params.id);
+    // fetch and set the project data
+    const { project } = await api.project.viewExpanded(params.id);
     if (!project) return console.log("No project found");
-    setProject(project);
 
-    // 2) Parse the home page from the project
-    const homePageId = project.connectedPages.find(
-      (page) => page.tilePage.name.toLowerCase() === "home"
-    )?.tilePageId;
-    if (!homePageId) return console.log("No home page found");
-
-    // 3) Fetch the home page by ID
-    const { page } = await api.project.viewPage(params.id, homePageId!);
-    if (!page) return console.log("No page found");
-    setPage(page);
+    setProject({
+      data: project,
+    });
+    setCurrentPageId(project.homePage);
   });
 
   return (
@@ -40,18 +38,22 @@ export default function Page() {
   );
 }
 
-const buttonPages = createMemo(() => {
-  const tiles = page()?.tilePage.tiles as Tile[];
-  if (!tiles) return [];
-
-  return tiles.reduce((acc, tile) => {
-    const pageIndex = tile.page || 0;
-    (acc[pageIndex] = acc[pageIndex] || []).push(tile);
-    return acc;
-  }, [] as Tile[][]);
-});
-
 function TilePageContainer() {
+  const buttonPages = createMemo(() => {
+    const currentPage = project.data?.pages[
+      currentPageId() as keyof typeof project.data.pages
+    ] as PageExpanded;
+
+    const tiles = Object.values(currentPage?.tiles || {});
+    if (!tiles) return [];
+
+    return tiles.reduce((acc, tile) => {
+      const pageIndex = tile.position.innerPageIndex || 0;
+      (acc[pageIndex] = acc[pageIndex] || []).push(tile);
+      return acc;
+    }, [] as TileExpanded[][]);
+  });
+
   const [containerRef, setContainerRef] = createSignal<HTMLElement | undefined>(
     undefined
   );
@@ -66,7 +68,7 @@ function TilePageContainer() {
         {containerSize().height}px
       </p>
       <div class="h-full w-full absolute top-0 left-0">
-        <Show when={page()}>
+        <Show when={project.data}>
           <For each={buttonPages()}>
             {(tiles, index) => (
               <PageSection
@@ -89,14 +91,14 @@ function PageSection({
 }: {
   index: () => number;
   containerSize: () => { height: number };
-  tiles: Tile[];
+  tiles: TileExpanded[];
 }) {
   return (
     <div
       style={{
         transform: `translateY(${index() * containerSize().height}px)`,
-        "grid-template-columns": `repeat(${project()!.columns}, 1fr)`,
-        "grid-template-rows": `repeat(${project()!.rows}, 1fr)`,
+        "grid-template-columns": `repeat(${project.data!.columns}, 1fr)`,
+        "grid-template-rows": `repeat(${project.data!.rows}, 1fr)`,
       }}
       class={cn("absolute top-0 left-0 w-full h-full grid p-2 gap-2", {
         "bg-zinc-200": index() % 2 !== 0,
@@ -106,20 +108,32 @@ function PageSection({
         {(tile) => (
           <button
             onClick={() => {
+              const navigateAction = tile.actions?.find(
+                (a) => a.type === "navigate"
+              );
+              if (navigateAction) {
+                const pageId = navigateAction.pageId;
+                if (pageId) {
+                  setCurrentPageId(pageId);
+                } else {
+                  console.error("No page ID found for navigation action");
+                }
+              }
+
               console.log("Tile clicked:", tile);
             }}
             class="h-full w-full relative brightness-100 hover:brightness-105 active:brightness-100 transition-all cursor-pointer"
             style={{
-              "grid-column": `${tile.x + 1} / span 1`,
-              "grid-row": `${tile.y + 1} / span 1`,
+              "grid-column": `${tile.position.x + 1} / span 1`,
+              "grid-row": `${tile.position.y + 1} / span 1`,
             }}
           >
             <div
               class={cn(
                 "transition grid border rounded-md overflow-hidden absolute top-0 left-0 w-full h-full",
                 {
-                  "grid-rows-[25%_75%] grid-cols-1": tile.image,
-                  "place-items-center": !tile.image,
+                  "grid-rows-[25%_75%] grid-cols-1": tile.mediaSrc,
+                  "place-items-center": !tile.mediaSrc,
                 }
               )}
               style={{
@@ -132,22 +146,22 @@ function PageSection({
                   <p
                     style="font-size: 25px;"
                     class={cn("text-[2vw] flex-1 truncate px-2 text-center", {
-                      "text-[25px]": tile.image,
+                      "text-[25px]": tile.mediaSrc,
                     })}
                   >
                     {tile.displayText || tile.text}
                   </p>
                 </div>
               </div>
-              <Show when={tile.image}>
+              <Show when={tile.mediaSrc}>
                 <img
-                  src={tile.image}
+                  src={tile.mediaSrc}
                   class="h-full w-full object-contain"
                   alt="Tile media"
                 ></img>
               </Show>
             </div>
-            <Show when={tile.navigation}>
+            <Show when={tile.actions?.find((a) => a.type === "navigate")}>
               <div
                 style={{
                   "background-color": tile.backgroundColor,
