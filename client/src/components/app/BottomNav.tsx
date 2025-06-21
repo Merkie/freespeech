c886c88eabe006df5a0d63f2aa96d88ecd746e08
merkie <merkie@post.com>
2025-06-21 18:27:35 -0500

    Refactor BottomNav component to use onClick handlers instead of href attributes for navigation

--- client/src/components/app/BottomNav.tsx
+++ client/src/components/app/BottomNav.tsx
@@ -1,16 +1,24 @@
 import { cn } from "@/lib/cn";
-import { A, useLocation } from "@solidjs/router";
-import { Accessor, Show } from "solid-js";
+import { project, setCurrentPageId } from "@/state";
+import { useLocation, useNavigate } from "@solidjs/router";
+import { Accessor } from "solid-js";
 
 export default function BottomNav() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <div class="grid grid-cols-3 grid-rows-1 gap-2 border border-x-0 border-b-0 border-zinc-700 bg-zinc-900 p-2 text-[25px] text-zinc-100">
       <NavButton
         selected={() => location.pathname.startsWith("/app/project")}
         icon="house-fill"
-        href="/app/project"
+        onClick={() => {
+          if (location.pathname.startsWith("/app/project")) {
+            setCurrentPageId(project.data!.homePage!);
+          } else {
+            navigate("/app/project/" + project.data!.id);
+          }
+        }}
       />
       <NavButton
         selected={() => false}
@@ -20,7 +28,9 @@ export default function BottomNav() {
       <NavButton
         selected={() => location.pathname.startsWith("/app/dashboard")}
         icon="gear-fill"
-        href="/app/dashboard/projects"
+        onClick={() => {
+          navigate("/app/dashboard/projects");
+        }}
       />
     </div>
   );
@@ -29,44 +39,24 @@ export default function BottomNav() {
 function NavButton({
   icon,
   selected,
-  href,
   onClick,
 }: {
   icon: string;
   selected: Accessor<boolean>;
-  href?: string;
   onClick?: () => void;
 }) {
   return (
-    <Show
-      when={href}
-      fallback={
-        <button
-          onClick={onClick}
-          class={cn(
-            "rounded-md w-full h-full p-1 text-center flex items-center justify-center transition-all",
-            {
-              "bg-zinc-800 opacity-100": selected(),
-              "opacity-50": !selected(),
-            }
-          )}
-        >
-          <i class={`bi bi-${icon}`}></i>
-        </button>
-      }
+    <button
+      onClick={onClick}
+      class={cn(
+        "rounded-md cursor-pointer w-full h-full p-1 text-center flex items-center justify-center transition-all",
+        {
+          "bg-zinc-800 opacity-100": selected(),
+          "opacity-50": !selected(),
+        }
+      )}
     >
-      <A
-        class={cn(
-          "rounded-md w-full h-full p-1 text-center flex items-center justify-center transition-all",
-          {
-            "bg-zinc-800 opacity-100": selected(),
-            "opacity-50": !selected(),
-          }
-        )}
-        href={href!}
-      >
-        <i class={`bi bi-${icon}`}></i>
-      </A>
-    </Show>
+      <i class={`bi bi-${icon}`}></i>
+    </button>
   );
 }
