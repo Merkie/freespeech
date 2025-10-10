46d1926bf8fa06062ef361ceeba258c3e34f30d8
Merkie <merkie@post.com>
2025-10-10 18:34:27 -0500

    refactor: consolidate ESLint ignore rules into the configuration file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,20 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
+	],
+	ignores: [
+		'.DS_Store',
+		'node_modules',
+		'/build',
+		'/.svelte-kit',
+		'/package',
+		'.env',
+		'.env.*',
+		'!.env.example',
+
+		// Ignore files for PNPM, NPM and YARN
+		'pnpm-lock.yaml',
+		'package-lock.json',
+		'yarn.lock'
 	]
 };
