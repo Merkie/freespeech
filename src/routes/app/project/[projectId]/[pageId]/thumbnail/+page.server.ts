df585834ed34106ce73861f0b7df431b89882323
Merkie <merkie@post.com>
2025-12-10 17:57:23 -0600

    Add server-side loading for project page data

---
+++ src/routes/app/project/[projectId]/[pageId]/thumbnail/+page.server.ts
@@ -0,0 +1,19 @@
+import api from '$ts/client/api/index.js';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ params: { projectId, pageId }, cookies }) => {
+	const { page: projectPage, isHomePage } = await api.project.viewPage(
+		projectId,
+		pageId,
+		cookies.get('token')
+	);
+
+	if (!projectPage || !projectPage.tilePage || !projectPage.project)
+		throw redirect(302, '/app/dashboard/projects');
+
+	return {
+		page: projectPage.tilePage,
+		project: projectPage.project,
+		isHomePage
+	};
+};
