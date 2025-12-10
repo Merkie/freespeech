c5ffb5e87fbf0101d091b00adbeac918bec95115
Merkie <merkie@post.com>
2025-12-10 17:57:27 -0600

    Refactor load function to use client-side data fetching instead of server-side API calls

--- src/routes/app/project/[projectId]/[pageId]/+layout.server.ts
+++ src/routes/app/project/[projectId]/[pageId]/+layout.server.ts
@@ -1,20 +1,6 @@
-import api from '$ts/client/api/index.js';
-import { redirect } from '@sveltejs/kit';
-
-export const load = async ({ params: { projectId, pageId }, cookies }) => {
-	const { page: projectPage, isHomePage } = await api.project.viewPage(
-		projectId,
-		pageId,
-		cookies.get('token')
-	);
-
-	if (!projectPage || !projectPage.tilePage || !projectPage.project)
-		throw redirect(302, '/app/dashboard/projects');
-
+export const load = async ({ params: { projectId, pageId } }) => {
 	return {
-		page: projectPage.tilePage,
-		project: projectPage.project,
-		projectPages: projectPage.project.connectedPages!.map((p) => p.tilePage),
-		isHomePage
+		projectId,
+		pageId
 	};
 };
